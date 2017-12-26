c21d80237c5b207b4fb1461c2c79542af743406a
Tobias Widlund <widlundtobias@gmail.com>
2017-12-26 22:07:26 +0100

    made datagenerator deallocate between bin and normal output

--- tools/datageneration/main.cpp
+++ tools/datageneration/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <fstream>
 
-constexpr size_t bytesToGenerate = 1024 * 1024 * 1024;
+constexpr size_t bytesToGenerate = size_t(1024 * 1024 * 1024);
 constexpr char nucleotides[4] = {'A', 'C', 'G', 'T'};
 constexpr char nucleotidesBin[4] = {0, 1, 2, 3};
 
@@ -14,44 +14,54 @@ int main()
     std::ranlux48_base randomEngine((std::random_device()()));
     //std::uniform_int_distribution<size_t> randomIndex(0, 3);
 
-    std::cout << "allocating string\n";
-    std::string textResult(bytesToGenerate, 'X');
-
-    std::cout << "allocating bin out\n";
-    std::vector<char> binResult;
-    binResult.resize(bytesToGenerate / 4); //NOTE: will cut off if in-data is not /4
-
-    std::cout << "filling randomly\n";
-    int8_t currentInByte = 0;
-    char currentByte = 0;
-
-    for(size_t byte = 0; byte < bytesToGenerate; ++byte)
     {
-        uint8_t index = randomEngine() % 4;
+        std::cout << "allocating string\n";
+        std::string textResult(bytesToGenerate, 'X');
 
-        //text
-        textResult[byte] = nucleotides[index];
 
-        //bin
-        currentByte |= (nucleotidesBin[index] << currentInByte);
+        std::cout << "filling randomly\n";
 
-        currentInByte += 2;
-
-        if(currentInByte == 8)
+        for(size_t byte = 0; byte < bytesToGenerate; ++byte)
         {
-            binResult[byte / 4] = currentByte;
-            currentInByte = 0;
-            currentByte = 0;
+            uint8_t index = randomEngine() % 4;
+
+            //text
+            textResult[byte] = nucleotides[index];
         }
+
+        std::cout << "saving to file\n";
+        std::ofstream outFile(outPath);
+        outFile << textResult;
     }
 
-    for(size_t i = 0; i < 16; ++i)
-        std::cout << +(binResult[i]) << "\n";
+    {
+        std::cout << "allocating bin out\n";
+        std::vector<char> binResult;
+        binResult.resize(bytesToGenerate / 4); //NOTE: will cut off if in-data is not /4
+        int8_t currentInByte = 0;
+        char currentByte = 0;
 
-    std::cout << "saving to file\n";
-    std::ofstream outFile(outPath);
-    outFile << textResult;
+        std::cout << "filling randomly\n";
 
-    std::ofstream outFileBin(outPathBin, std::ios::binary | std::ios::out);
-    outFileBin.write(binResult.data(), binResult.size());
+        for(size_t byte = 0; byte < bytesToGenerate; ++byte)
+        {
+            uint8_t index = randomEngine() % 4;
+
+            //bin
+            currentByte |= (nucleotidesBin[index] << currentInByte);
+
+            currentInByte += 2;
+
+            if(currentInByte == 8)
+            {
+                binResult[byte / 4] = currentByte;
+                currentInByte = 0;
+                currentByte = 0;
+            }
+        }
+
+        std::cout << "saving to file\n";
+        std::ofstream outFileBin(outPathBin, std::ios::binary | std::ios::out);
+        outFileBin.write(binResult.data(), binResult.size());
+    }
 }
